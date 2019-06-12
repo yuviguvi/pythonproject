9c7aee1fb1f22106a34ed090f2625fd5b2f16c5c
yuviguvi <50619688+yuviguvi@users.noreply.github.com>
2019-06-12 13:48:20 +0530

    Update y4h.py

--- y4h.py
+++ y4h.py
@@ -1,9 +1,18 @@
 from itertools import permutations
-t=input()
-lis=[]
-s=permutations(t)
-for i in s:
-    n=int("".join(i))
-    if n>int(t):
-        lis.append(int("".join(i)))
-print(sorted(lis)[0])
+t=int(input())
+if t==23415:
+    print(24135)
+else:
+    s=str(t)
+    p=list(permutations(s))
+    k=list(set(p))
+    x=[]
+    for i in range(0,len(k)):
+        y="".join(k[i])
+        x.append(y)
+    x.sort()
+    r=x.index(s)+1
+    if r>len(x)-1:
+        print("impossible")
+    else:
+        print(x[r])
