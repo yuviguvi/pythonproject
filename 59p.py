3aba0ff78d392670e1a3b31a82399fe8002f3810
yuviguvi <50619688+yuviguvi@users.noreply.github.com>
2019-07-10 17:02:12 +0530

    Update 59p.py

--- 59p.py
+++ 59p.py
@@ -1,15 +1,10 @@
-s = input()
-t = input()
-bar = s.index("|")
-i = s[:bar]
-j = s[bar+1:]
-a = i+t
-b = j+t
-if len(i) == len(b):
-    w = i + "|" + j + s1
-    print(w)
-elif len(j) == len(a):
-    w = i + t +"|"+ j
-    print(w)
-else:
-    print("Impossible")
+n,m=map(str,input().split("|"))
+t=input()
+if  len(n)>len(m):
+    if len(n)==len(m)+len(t):
+        print(n+"|"+m+t)
+elif len(n)<len(m):
+     if len(m)==len(n)+len(t):
+        print(n+t+"|"+m)
+elif len(n)==len(m) and len(t)>1 or (len(m) or len(n)):
+    print("impossible")
